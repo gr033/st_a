42aa163f451829eb451bb27168cbdfb1b0ea35b3
seon1112 <vosej7930@gmail.com>
2023-07-24 16:36:48 +0900

    second commit

--- st_a/src/main/java/com/example/demo/controller/DeptController.java
+++ st_a/src/main/java/com/example/demo/controller/DeptController.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.dao.DeptDAO_JPA;
 import com.example.demo.dao.DeptDAO_MB;
+import com.example.demo.entity.*;
+
 
 import lombok.Setter;
 
@@ -18,6 +21,17 @@ public class DeptController {
 	private DeptDAO_JPA dao_JPA;
 	@Autowired
 	private DeptDAO_MB dao_MB;
+	
+	@GetMapping("/dept/insert")
+	public void insert() {
+		
+	}
+	
+	@PostMapping("/dept/insert")
+	public String insert(Dept d) {
+		dao_JPA.save(d);
+		return "redirect:/dept/list";
+	}
 	
 	@GetMapping("/dept/list")
 	public void list(Model model) {
